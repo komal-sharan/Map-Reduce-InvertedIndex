e6c6a2b84f64bfd16dd446b8cb3dd36ee9ff009a
Komal Sharan <komalssharan@gmail.com>
2016-12-04 11:19:36 -0500

    working version

--- invertedindexnew.java
+++ invertedindexnew.java
@@ -1,6 +1,5 @@
 
 package invertedindex;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -23,28 +22,35 @@ public class invertedindex {
 
 			br = new BufferedReader(new FileReader(csvFile));
 			// have to put in a while loop
+			
 			while ((line = br.readLine()) != null) {
 
 				String[] country = line.split(cvsSplitBy);
+				System.out.println(country[1]);
+				System.out.println(country[0]);
 				tweetid++;
+				
 				System.out.println("tweetid" + tweetid);
 				// converting integer tweet to string
 				String tweetidtostring = Integer.toString(tweetid);
 				// putting in the hashmap which maps the object id to tweetid
 				mapping.put(country[0], tweetidtostring);
 				country[1]=country[1]+"|";
-				System.out.println(country[1]);
+				
+				
 				// putting in the hashmap which maps the tweet to the tweetid
 
 				mappingtweetoId.put(country[1], tweetidtostring);
-
+              
 				int j = 0;
 				int i = 0;
 
-				for (String key : mappingtweetoId.keySet()) {
-
-					while (key.charAt(j) != '|') {
-
+		/*		for (String key : mappingtweetoId.keySet()) {*/
+                    String  key=country[1];
+                  //  System.out.println(key.length());
+					
+						i++;
+						//System.out.println(key.length());
 						String[] words = key.split("\\s+");
 						for (i = 0; i < words.length; i++) {
 
@@ -63,39 +69,34 @@ public class invertedindex {
 								h.put(words[i], list1);
 							}
 
-							country[0] = "";
-							country[1] = "";
-							for (HashMap.Entry<String, ArrayList<String>> entry : h.entrySet()) {
-								System.out.println(entry.getKey() + " : " + entry.getValue() + tweetid);
-							}
-
+							
+							
 						}
-
-					}
-
-				}
+						
+						
 
 			}
+			for (HashMap.Entry<String, ArrayList<String>> entry : h.entrySet()) {
+				System.out.println(entry.getKey() + " : " + entry.getValue());
+			}
+			
 
 		}
 
-		catch (FileNotFoundException e) {
+	 catch (FileNotFoundException e) {
+         e.printStackTrace();
+     } catch (IOException e) {
+         e.printStackTrace();
+     } finally {
+         if (br != null) {
+             try {
+             	br.close();
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+         }
+     }
 
-			e.printStackTrace();
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-
-					e.printStackTrace();
-				}
-			}
-		}
-
-	}
+ }
 
 }
